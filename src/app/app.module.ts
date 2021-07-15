ef0331691815d7b56a0af20c5c46c9b86a6e9272
erjonlusha <erjonlusha10@gmail.com>
2021-07-15 09:32:55 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Routing module for router service
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+
+// HttpClient module for RESTful API
+import { HttpClientModule } from '@angular/common/http';
+
+// Forms module
+import { FormsModule } from '@angular/forms';
+
+//Components
+import { EmployeeCreateComponent } from './employee-create/employee-create.component';
+import { EmployeeEditComponent } from './employee-edit/employee-edit.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeCreateComponent,
+    EmployeeEditComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
